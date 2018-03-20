4a416e2ce14d10e250fc42c51b00d2a19789d324
Evelyn Masso <evelyn@carbonfive.com>
2018-03-20 11:16:03 -0700

    serve it

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,19 @@
 const Koa = require('koa');
 const app = new Koa();
 
+const Pug = require('koa-pug');
+const pug = new Pug({
+  app,
+  viewPath: './build',
+  pretty: true,
+});
+
+const serve = require('koa-static');
+
+app.use(serve('build/client'));
+
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+  ctx.render('index.pug');
 });
 
 app.listen(3000);
